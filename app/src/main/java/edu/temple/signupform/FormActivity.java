651fe5e6ca482b0439656f8c452a398d52825c11
pete5431 <scholarpetez@gmail.com>
2020-02-12 16:46:17 -0500

    Added conditionals for name and email edit text if they are empty or just contain spaces

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -2,9 +2,13 @@ package edu.temple.signupform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -12,6 +16,8 @@ public class FormActivity extends AppCompatActivity {
     EditText editEmail;
     EditText editPassword;
     EditText editConfirmPassword;
+    Button saveButton;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +28,20 @@ public class FormActivity extends AppCompatActivity {
         editEmail = findViewById(R.id.editEmail);
         editPassword = findViewById(R.id.editPassword);
         editConfirmPassword = findViewById(R.id.editConfirmPass);
+        saveButton = findViewById(R.id.saveButton);
 
+        context = getApplicationContext();
 
-
-
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editName.getText().toString().trim().isEmpty()){
+                    Toast.makeText(context, "Please enter name", Toast.LENGTH_SHORT).show();
+                }
+                else if(editEmail.getText().toString().trim().isEmpty()){
+                    Toast.makeText(context, "Please enter email", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
