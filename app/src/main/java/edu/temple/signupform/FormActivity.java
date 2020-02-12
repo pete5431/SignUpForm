f758ac297127381e14715b61fe6e9a58d750f597
pete5431 <scholarpetez@gmail.com>
2020-02-12 16:34:22 -0500

    Added Save button

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,13 +3,26 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FormActivity extends AppCompatActivity {
+
+    EditText editName;
+    EditText editEmail;
+    EditText editPassword;
+    EditText editConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        editName = findViewById(R.id.editName);
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
+        editConfirmPassword = findViewById(R.id.editConfirmPass);
+
 
 
 
